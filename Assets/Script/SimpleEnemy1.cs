003f5e73572c5c8ca4c75336198cdba35379ee63
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2024-08-04 22:26:59 +0900

    Attack 시범 구현

--- Assets/Script/SimpleEnemy1.cs
+++ Assets/Script/SimpleEnemy1.cs
@@ -7,11 +7,21 @@ public class SimpleEnemy1 : Enemy
     void Start(){
         hp = 10f;
         damage = 10f;
+        StartCoroutine(Attack1());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator Attack1()
+    {
+        while(true){
+            Instantiate(attack1, transform.position, Quaternion.identity);
+
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
