6a959d82a1dc930b1fd91a5300d6d92315a7d092
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2024-08-15 09:12:41 +0900

    작업중4

--- Assets/Script/anim/Cave1.cs
+++ Assets/Script/anim/Cave1.cs
@@ -2,29 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.Timeline;
 
 public class Cave1 : MonoBehaviour
 {
-    PlayableDirector pd;
+    [SerializeField] private PlayableDirector pd;
+    [SerializeField] private TimelineAsset ta;
     void Start()
     {
-        Debug.Log("feiadf");
-        pd = GetComponent<PlayableDirector>();
         StartCoroutine("ExitTimeline");
-        Debug.Log("dfes");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     IEnumerator ExitTimeline()
     {
-        Debug.Log("dfa");
+        pd.Play(ta);
         yield return new WaitForSeconds(9f);
         GameObject.FindWithTag("Player").GetComponent<Animator>().enabled = false;
-        Debug.Log("dfasefdsfaaefa");
     }
 }
