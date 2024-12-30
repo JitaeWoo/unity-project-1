4a090a9a13837a6ab2cd4aba6765ffb0b198bbda
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2024-12-31 00:28:21 +0900

    구조 개선중...

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -6,33 +6,28 @@ using UnityEngine.UI;
 
 public class PlayerController : Singleton<PlayerController>
 {
+    [SerializeField] private PlayerBaseStats _baseStats;
     [SerializeField] private Slider _hpBar;
+    private PlayerStats _stats;
     private PlayerHealth _hp;
     private PlayerHealthUI _hpUI;
     private PlayerMovement _movement;
     private PlayerInputHandler _inputHandler;
     private PlayerDamageHandler _damageHandler;
-    private float _speed;
-
-    public float damage;
-    private float invicibleTime;
-    private bool isInvincible;
+    private PlayerInvincibilityManager _invincibilityManager;
     private bool isControll;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("I started!");
-        _hp = new PlayerHealth(100);
+        _stats = new PlayerStats(_baseStats);
+        _hp = new PlayerHealth(_stats.MaxHealth);
         _hpUI = new PlayerHealthUI(_hpBar);
         _movement = new PlayerMovement(transform);
         _inputHandler = new PlayerInputHandler();
         _damageHandler = new PlayerDamageHandler(_hp, _hpUI);
-
-        _speed = 5f;
-        damage = 10f;
-        invicibleTime = 0.5f;
-        isInvincible = false;
+        _invincibilityManager = new PlayerInvincibilityManager(this, 0.5f);
         isControll = true;
     }
 
@@ -42,22 +37,18 @@ public class PlayerController : Singleton<PlayerController>
         if (isControll)
         {
             Vector3 direction = _inputHandler.GetMovementInput();
-            _movement.Move(direction, _speed);
+            _movement.Move(direction, _stats.Speed);
         }
     }
 
     void OnTriggerStay2D(Collider2D other)
     {
-        _damageHandler.HandleDamage(other, isInvincible);
-    }
+        _damageHandler.HandleDamage(other, _invincibilityManager.IsInvincible);
 
-    private IEnumerator InvincibleDuration(float ITime)
-    {
-        isInvincible = true;
-
-        yield return new WaitForSeconds(ITime);
-
-        isInvincible = false;
+        if (!_invincibilityManager.IsInvincible)
+        {
+            _invincibilityManager.StartInvincibility();
+        }
     }
 
     public void SetIsControll(bool set)
